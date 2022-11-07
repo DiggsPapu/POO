301a2ba80a7bc35d7e6c4d480d38aa3aede29ea8
Your Name <alo20172@uvg.edu.gt>
2022-11-07 07:18:37 -0600

    Javadoc Users

--- src/users/AdminUser.java
+++ src/users/AdminUser.java
@@ -12,84 +12,127 @@ public class AdminUser implements User {
     private boolean in_meeting = false;
     private ArrayList<String> inbox = new ArrayList<>();
     /** This are the overrides from the interface */
+    /**
+     * To get name
+     * @return
+     */
     @Override
     public String getName() {
         // TODO Auto-generated method stub
         return name;
     }
-
+    /**
+     * To set the name
+     * @param name
+     */
     @Override
     public void setName(String name) {
         // TODO Auto-generated method stub
         this.name = name;
     }
-
+    /**
+     * To get the id
+     * @return
+     */
     @Override
     public int getId() {
         // TODO Auto-generated method stub
         return id;
     }
-
+    /**
+     * To set the id
+     * @param value
+     */
     @Override
     public void setId(int value) {
         // TODO Auto-generated method stub
         this.id = value;
     }
-
+    /**
+     * To get the device
+     * @return
+     */
     @Override
     public Device getDevice() {
         // TODO Auto-generated method stub
         return device;
     }
-
+    /**
+     * To set the device
+     * @param value
+     */
     @Override
     public void setDevice(Device value) {
         // TODO Auto-generated method stub
         this.device = value;
     }
-
+    /**
+     * To get if the camera is active
+     * @return
+     */
     @Override
     public boolean getCamera() {
         // TODO Auto-generated method stub
         return camera;
     }
-
+    /**
+     * To set the activation of camera
+     * @param value
+     */
     @Override
     public void setCamera(boolean value) {
         // TODO Auto-generated method stub
         camera = value;
     }
-
+    /**
+     * To get if the microphone is active
+     * @return
+     */
     @Override
     public boolean getMicro() {
         // TODO Auto-generated method stub
         return micro;
     }
-
+    /**
+     * To set the activation of the microphone
+     * @param value
+     */
     @Override
     public void setMicro(boolean value) {
         // TODO Auto-generated method stub
         micro = value;
     }
-
+    /**
+     * To get if is in a meeting
+     * @return
+     */
     @Override
     public boolean isInMeeting() {
         // TODO Auto-generated method stub
         return in_meeting;
     }
-
+    /**
+     * Set if meeting status
+     * @param value
+     */
     @Override
     public void setInMeeting(boolean value) {
         // TODO Auto-generated method stub
         in_meeting = value;
     }
-
+    /**
+     * To get the inbox
+     * @return
+     */
     @Override
     public ArrayList<String> getInbox() {
         // TODO Auto-generated method stub
         return inbox;
     }
-
+    /**
+     * To set the inbox
+     * @param inbox
+     */
     @Override
     public void setInbox(ArrayList<String> inbox) {
         // TODO Auto-generated method stub
--- src/users/User.java
+++ src/users/User.java
@@ -5,18 +5,74 @@ import java.util.ArrayList;
 import device.Device;
 
 public interface User {
+    /**
+     * To get name
+     * @return
+     */
     public String getName();
+    /**
+     * To set the name
+     * @param name
+     */
     public void setName(String name);
+    /**
+     * To get the id
+     * @return
+     */
     public int getId();
+    /**
+     * To set the id
+     * @param value
+     */
     public void setId(int value);
+    /**
+     * To get the device
+     * @return
+     */
     public Device getDevice();
+    /**
+     * To set the device
+     * @param value
+     */
     public void setDevice(Device value);
+    /**
+     * To get if the camera is active
+     * @return
+     */
     public boolean getCamera();
+    /**
+     * To set the activation of camera
+     * @param value
+     */
     public void setCamera(boolean value);
+    /**
+     * To get if the microphone is active
+     * @return
+     */
     public boolean getMicro();
+    /**
+     * To set the activation of the microphone
+     * @param value
+     */
     public void setMicro(boolean value);
+    /**
+     * To get if is in a meeting
+     * @return
+     */
     public boolean isInMeeting();
+    /**
+     * Set if meeting status
+     * @param value
+     */
     public void setInMeeting(boolean value);
+    /**
+     * To get the inbox
+     * @return
+     */
     public ArrayList<String> getInbox();
+    /**
+     * To set the inbox
+     * @param inbox
+     */
     public void setInbox(ArrayList<String> inbox);    
 }
